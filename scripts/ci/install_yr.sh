c6b9e24bcc1b3754458620b1f0d13b074ab3b6a6
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-18 07:05:45 -0400

    fix: remove sudo from install command in install script

--- scripts/ci/install_yr.sh
+++ scripts/ci/install_yr.sh
@@ -10,6 +10,6 @@ fi
 URL="https://github.com/mikefarah/yq/releases/latest/download/yq_linux_${YQ_ARCH}"
 
 curl -fsSL "$URL" -o /tmp/yq
-sudo install -m 0755 /tmp/yq /usr/local/bin/yq
+install -m 0755 /tmp/yq /usr/local/bin/yq
 echo "YQ version"
 yq --version
