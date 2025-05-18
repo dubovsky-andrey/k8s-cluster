d3a0296d712f9958800795672cbd574ca4ebbeef
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-18 06:55:04 -0400

    fix: update yq download URL to always fetch the latest release

--- scripts/ci/install_yr.sh
+++ scripts/ci/install_yr.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-YQ_VER="v4.44.1"
 ARCH=$(uname -m)
 if [ "$ARCH" = "x86_64" ]; then
   YQ_ARCH="amd64"
 else
   YQ_ARCH="arm64"
 fi
-curl -L "https://github.com/mikefarah/yq/releases/download/${YQ_VER}/yq_linux_${YQ_ARCH}" -o /tmp/yq
-install -m 0755 /tmp/yq /usr/local/bin/yq
+
+# Эта ссылка всегда отдаёт последний релиз yq для нужной архитектуры
+URL="https://github.com/mikefarah/yq/releases/latest/download/yq_linux_${YQ_ARCH}"
+
+curl -fsSL "$URL" -o /tmp/yq
+sudo install -m 0755 /tmp/yq /usr/local/bin/yq
+echo "YQ version"
+yq --version
