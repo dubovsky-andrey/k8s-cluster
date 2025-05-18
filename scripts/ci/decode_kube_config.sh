535e3722190d52beb0a8d762822daa3986ca1bac
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-05-18 07:34:22 -0400

    fix: update kubeconfig file path for consistency and clarity

--- scripts/ci/decode_kube_config.sh
+++ scripts/ci/decode_kube_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Decode admin kubeconfig from secret
-echo "${{ secrets.ADMIN_KUBECONFIG_B64 }}" | base64 -d > $HOME/kubeconfig
-chmod 600 $HOME/kubeconfig
+echo "$ADMIN_KUBECONFIG_B64" | base64 -d > "$HOME/.kube/config"
+chmod 600 "$HOME/.kube/config"
 # Export for all subsequent steps
 echo "KUBECONFIG=$HOME/kubeconfig" >> $GITHUB_ENV
